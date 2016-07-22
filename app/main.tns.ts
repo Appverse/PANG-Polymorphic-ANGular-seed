c8bbbe3d4ab820c0de9f110c2854a4b1a99c138c
orgbx <orgb@gft.com>
2016-07-22 12:06:19 +0200

    evergreen seed (upgrading to RC4)

--- app/main.tns.ts
+++ app/main.tns.ts
@@ -1,10 +1,10 @@
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {nsProvideRouter} from "nativescript-angular/router";
-import {HTTP_PROVIDERS} from '@angular/http';
+import {NS_HTTP_PROVIDERS} from 'nativescript-angular/http';
 import {AppComponent, APP_ROUTES} from "./client/app.component";
 
 nativeScriptBootstrap(AppComponent, [
   nsProvideRouter(APP_ROUTES, { enableTracing: false }),
-  HTTP_PROVIDERS]
-).catch(err => console.error(err));
+  NS_HTTP_PROVIDERS]
+)
 
