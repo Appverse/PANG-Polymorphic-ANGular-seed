3884f984b60cfa2eaeb03c01058aaf1408af4eb5
carlosrubio <carlos.rubio@gft.com>
2016-09-19 23:35:49 +0200

    use ElementReft instead of less clear $event

--- app/client/pages/products/products.component.ts
+++ app/client/pages/products/products.component.ts
@@ -27,14 +27,15 @@ export class ProductsComponent {
             .subscribe(product => this.loadedProduct = product);
     }
 
+
     /**
      * Sometimes, components can be almost fully reused between platforms
      * global.android and global.web can help implement small platform specific logic
      */
-    remove(product: models.Product, event?: any) {
+    remove(product: models.Product, productElement?: ElementRef) {
         if (global.android) {
             let explosion = require('nativescript-explosionfield');
-            explosion.explode(event.object._parent._parent);
+            explosion.explode(productElement);
             setTimeout(() => {
                 this.removeProduct.call(this, product.id);
             }, 500);
@@ -42,6 +43,7 @@ export class ProductsComponent {
             this.removeProduct.call(this, product.id);
         }
     }
+
 
     private removeProduct(id: number) {
         for (var index = 0; index < this.products.length; index++) {
